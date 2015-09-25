36d32a27b745cbdd2f2d35fe0a8f6938a64c0a3b
Rudd Fawcett <rudd.fawcett@gmail.com>
2015-09-25 08:27:21 -0400

    Added MathOperationTests.java

---
+++ fall/class/MathOperationTests.java
@@ -0,0 +1,96 @@
+/**
+* MathOperationTests
+* Runs basic math operation tests for int, float and double.
+*
+* @author  Rudd Fawcett
+* @version 1.0
+* @since   2015-09-25
+*/
+
+public class MathOperationTests {
+  public static void main(String[] args) {
+    // BEGIN int testing.
+    println("int Division Test: " + divide(7, 2));
+    println("int Multiplication Test: " + multiply(7, 2));
+    println("int Addition Test: " + add(7, 2));
+    println("int Subtraction Test: " + subtract(7, 2));
+
+    println("");
+
+    // BEGIN float testing.
+
+    println("float Division Test: " + floatDivide(7, 2));
+    println("float Multiplication Test: " + floatMultiply(7, 2));
+    println("float Addition Test: " + floatAdd(7, 2));
+    println("float Subtraction Test: " + floatSubtract(7, 2));
+
+    println("");
+
+    // BEGIN double testing.
+
+    println("double Division Test: " + doubleDivide(7.5, 2.3));
+    println("double Multiplication Test: " + doubleMultiply(7.5, 2.3));
+    println("double Addition Test: " + doubleAdd(7.5, 2.3));
+    println("double Subtraction Test: " + doubleSubtract(7.5, 2.3));
+  }
+
+  // HELPERS
+
+  public static void println(Object input) {
+    System.out.println(input);
+  }
+
+  // INT
+
+  public static int add(int first, int second) {
+    return first+second;
+  }
+
+  public static int subtract(int first, int second) {
+    return first - second;
+  }
+
+  public static int divide(int first, int second) {
+    return first/second;
+  }
+
+  public static int multiply(int first, int second) {
+    return first*second;
+  }
+
+  // FLOAT
+
+  public static float floatAdd(float first, float second) {
+    return first+second;
+  }
+
+  public static float floatSubtract(float first, float second) {
+    return first - second;
+  }
+
+  public static float floatDivide(float first, float second) {
+    return first/second;
+  }
+
+  public static float floatMultiply(float first, float second) {
+    return first*second;
+  }
+
+  // DOUBLE
+
+  public static double doubleAdd(double first, double second) {
+    return first+second;
+  }
+
+  public static double doubleSubtract(double first, double second) {
+    return first - second;
+  }
+
+  public static double doubleDivide(double first, double second) {
+    return first/second;
+  }
+
+  public static double doubleMultiply(double first, double second) {
+    return first*second;
+  }
+}
