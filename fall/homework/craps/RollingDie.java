14d1496f4cf4917e2f96fb94f2e31ee7e2f15bd0
Rudd Fawcett <rudd.fawcett@gmail.com>
2015-10-19 08:08:46 -0400

    Fixed indenting on RollingDie switch statement

--- fall/homework/craps/RollingDie.java
+++ fall/homework/craps/RollingDie.java
@@ -157,39 +157,39 @@ public class RollingDie extends Die
     int y3 = y + 5*step + 1;
 
     switch (numDots) {
-    case 1:
-      g.fillOval(x2, y2, dotSize, dotSize);
-      break;
-    case 2:
-      g.fillOval(x1, y1, dotSize, dotSize);
-      g.fillOval(x3, y3, dotSize, dotSize);
-      break;
-    case 3:
-      g.fillOval(x1, y1, dotSize, dotSize);
-      g.fillOval(x2, y2, dotSize, dotSize);
-      g.fillOval(x3, y3, dotSize, dotSize);
-      break;
-    case 4:
-      g.fillOval(x1, y1, dotSize, dotSize);
-      g.fillOval(x1, y3, dotSize, dotSize);
-      g.fillOval(x3, y1, dotSize, dotSize);
-      g.fillOval(x3, y3, dotSize, dotSize);
-      break;
-    case 5:
-      g.fillOval(x1, y1, dotSize, dotSize);
-      g.fillOval(x1, y3, dotSize, dotSize);
-      g.fillOval(x2, y2, dotSize, dotSize);
-      g.fillOval(x3, y1, dotSize, dotSize);
-      g.fillOval(x3, y3, dotSize, dotSize);
-      break;
-    case 6:
-      g.fillOval(x1, y1, dotSize, dotSize);
-      g.fillOval(x1, y2, dotSize, dotSize);
-      g.fillOval(x1, y3, dotSize, dotSize);
-      g.fillOval(x3, y1, dotSize, dotSize);
-      g.fillOval(x3, y2, dotSize, dotSize);
-      g.fillOval(x3, y3, dotSize, dotSize);
-      break;
+      case 1:
+        g.fillOval(x2, y2, dotSize, dotSize);
+        break;
+      case 2:
+        g.fillOval(x1, y1, dotSize, dotSize);
+        g.fillOval(x3, y3, dotSize, dotSize);
+        break;
+      case 3:
+        g.fillOval(x1, y1, dotSize, dotSize);
+        g.fillOval(x2, y2, dotSize, dotSize);
+        g.fillOval(x3, y3, dotSize, dotSize);
+        break;
+      case 4:
+        g.fillOval(x1, y1, dotSize, dotSize);
+        g.fillOval(x1, y3, dotSize, dotSize);
+        g.fillOval(x3, y1, dotSize, dotSize);
+        g.fillOval(x3, y3, dotSize, dotSize);
+        break;
+      case 5:
+        g.fillOval(x1, y1, dotSize, dotSize);
+        g.fillOval(x1, y3, dotSize, dotSize);
+        g.fillOval(x2, y2, dotSize, dotSize);
+        g.fillOval(x3, y1, dotSize, dotSize);
+        g.fillOval(x3, y3, dotSize, dotSize);
+        break;
+      case 6:
+        g.fillOval(x1, y1, dotSize, dotSize);
+        g.fillOval(x1, y2, dotSize, dotSize);
+        g.fillOval(x1, y3, dotSize, dotSize);
+        g.fillOval(x3, y1, dotSize, dotSize);
+        g.fillOval(x3, y2, dotSize, dotSize);
+        g.fillOval(x3, y3, dotSize, dotSize);
+        break;
     }
   }
 }
